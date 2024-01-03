73bc4b9409aecfc9f4e550b66004f5404d6b4ff7
Cao Ke <caokecs@outlook.com>
2024-01-04 02:28:31 +0800

    day2

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question
+
+admin.site.register(Question) #注册Question模型
+
+# admin.site 是一个Django自带的管理站点，它可以让你在一个地方管理所有注册的模型。
+# admin.site.register(Question)告诉管理站点，Question对象需要被管理。默认情况下，Django为每个模型生成一个表单。
